a8e2edfb05c26928dc786564ad2b9fd79c60318a
Joe Meissler <joe.meissler@gmail.com>
2023-03-15 20:51:28 -0700

    use nvim-autopairs instead of mini

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -127,10 +127,17 @@ return {
 
   -- auto pairs
   {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
+    "windwp/nvim-autopairs",
+    opts = {
+      fast_wrap = {},
+      disable_filetype = { "TelescopePrompt", "vim" },
+    },
     config = function(_, opts)
-      require("mini.pairs").setup(opts)
+      require("nvim-autopairs").setup(opts)
+
+      -- setup cmp for autopairs
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
 
